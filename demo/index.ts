2b795e6dc599f5b7712b3df62e6a97e9ecb1d9c8
quark <1523838521@qq.com>
2024-11-21 15:09:04 +0800

    feat: 调整压缩策略

--- demo/index.ts
+++ demo/index.ts
@@ -1,38 +1,38 @@
 import { compress } from "../src";
 
 // 添加一个图片上传按钮
-const input = document.createElement('input');
-input.type = 'file';
-input.accept = 'image/*';
+const input = document.createElement("input");
+input.type = "file";
+input.accept = "image/*";
 input.multiple = true;
 document.body.appendChild(input);
 
-const container = document.createElement('div');
+const container = document.createElement("div");
 document.body.appendChild(container);
 
 input.onchange = async (): Promise<void> => {
     try {
-        console.time('compress');
+        console.time("compress");
 
         if (!input.files) return;
-        const blobs = await compress(input.files);
-        console.timeEnd('compress');
+        const blobs = await compress(input.files, { useWebp: false });
+        console.timeEnd("compress");
         // 显示成功
         document.body.append("压缩成功");
         // 下载 blob
         for (let i = 0; i < blobs.length; i++) {
             const blob = blobs[i];
             const url = URL.createObjectURL(blob);
-            const a = document.createElement('a');
+            const a = document.createElement("a");
             a.href = url;
-            a.download = `compressed-${i}.webp`;
+            a.download = `compressed-${i}.${blob.type.split("/")[1]}`;
             a.textContent = `下载-${i + 1}`;
             container.appendChild(a);
         }
     } catch (error) {
-        console.error('压缩失败', error);
+        console.error("压缩失败", error);
     }
-}
+};
 
 // 页面流畅性检测
 let time = new Date().getTime();
@@ -40,10 +40,9 @@ const show = () => {
     const cur = new Date().getTime();
     const timeDiff = cur - time;
     if (timeDiff > 1000) {
-        console.log('time:', timeDiff);
+        console.log("time:", timeDiff);
     }
     time = cur;
-    requestAnimationFrame(show)
-}
+    requestAnimationFrame(show);
+};
 show();
-
--- src/compress.ts
+++ src/compress.ts
@@ -17,31 +17,48 @@ export interface CompressOptions {
     fileSizeLimit?: number;
 
     /**
-     * 图片尺寸限制，单位 px
+     * 图片尺寸限制，单位 px，不应超过浏览器上限 16383
      * @default 8192
      */
     lenSizeLimit?: number;
 
     /**
      * 压缩质量
-     * @default 0.8
+     * @default 0.9
      */
     quality?: number;
 
     /**
      * 是否使用 webp 格式
+     * @default true
      */
     useWebp?: boolean;
 }
 
 export async function compressImg(file: File, options?: CompressOptions): Promise<Blob> {
-    const { type } = file;
+    // const { type: originType, size: originSize } = file;
     const bitmap = await createImageBitmap(file);
     let { width, height } = bitmap;
-    let { quality = 0.8, lenSizeLimit = 8192, fileSizeLimit: FileSizeLimit = 30, useWebp = true } = options || {};
-    const format = useWebp ? 'image/webp' : type;
+    let { quality = 0.9, lenSizeLimit = 8192, fileSizeLimit = 30, useWebp = true } = options || {};
+    /**计算图片解码后的理论大小 */
+    // const deComposedSize  = width * height * 4;
+    /**计算原本的压缩比 */
+    // const originCompressRate = originSize / deComposedSize;
+    // const isQualityOptimized = originCompressRate < 0.5;
+
+    // let isTypeOptimized = false;
+    // let isSizeOptimized = false;
+    /**如果原本的压缩比小于 0.5，说明图片已经被压缩过，不再通过降低质量压缩，而是类型 和 尺寸 */
+    // if (isQualityOptimized) {
+    //     isTypeOptimized = true;
+    //     isSizeOptimized = true;
+    // }
+    /**计算还需压缩多少 */
+    // const remainSize = originSize / (fileSizeLimit << 20);
+
+    let type = useWebp ? "image/webp" : "image/jpeg";
     /**max size 最大文件体积 */
-    const mSize = FileSizeLimit << 20;
+    const mSize = fileSizeLimit << 20;
     const area = width * height;
 
     /**max len 最大尺寸 */
@@ -49,21 +66,19 @@ export async function compressImg(file: File, options?: CompressOptions): Promis
     let scale = 1;
     if (area > MaxArea || width > lenSizeLimit || height > lenSizeLimit) {
         scale = Math.min(Math.sqrt(MaxArea / area), mLen / width, mLen / height);
-        width = Math.floor(width * scale);
-        height = Math.floor(height * scale);
     }
-
-    const canvas = new OffscreenCanvas(width, height);
-    const ctx = canvas.getContext('2d')!;
-    ctx.scale(scale, scale);
-    ctx.drawImage(bitmap, 0, 0);
 
     let blob: Blob;
     do {
-        blob = await canvas.convertToBlob({ type: format, quality });
+        width = Math.floor(width * scale);
+        height = Math.floor(height * scale);
+        const canvas = new OffscreenCanvas(width, height);
+        const ctx = canvas.getContext("2d")!;
+        ctx.scale(scale, scale);
+        ctx.drawImage(bitmap, 0, 0);
+        blob = await canvas.convertToBlob({ type, quality });
         // bold size 越大，质量越差
-        quality *= (mSize / blob.size - 0.1);
-    }
-    while (blob.size > mSize && quality > 0.1);
+        scale *= Math.sqrt(mSize / blob.size);
+    } while (blob.size > mSize && quality > 0.1);
     return blob;
 }
